2ec73e1c534365fe73b529573265724e4eb57d3b
palak-kaushal <kaushalpalak2005@gmail.com>
2025-06-25 15:16:33 +0530

    add files

---
+++ day3.js
@@ -0,0 +1,83 @@
+// var a=10;
+// var a=20
+// console.log(a)
+// let a=10;
+// a =30
+// console.log(a)
+
+// const a=10;
+// console.log(a)
+//this is function declation
+// let xyz =abc (2,4)
+// console.log(xyz)
+
+// function abc(a,b){
+//     return a+b
+// }
+//function expression
+// const abc =function(a,b){
+//     return a+b
+// }
+// const xyz = abc(8,4)
+// console.log(xyz)
+
+// const abc =(a,b) =>{
+//     return a+b
+
+// }
+// const xyz =abc(10,20)
+// console.log(xyz)
+
+// console.log(a)
+// var a =10;
+// console.log(a)
+// var a =10;
+// console.log(a)
+// const a =10;
+
+// var a=10;
+// function abc(){
+//     var a=20;
+//     console.log(a)
+// }
+// console.log(a)
+// abc()
+
+// var a= 10
+// if(a==10){
+//     var b =20;
+//     console.log(b)
+
+// }
+// console.log(b)
+
+// function abc(){
+//     let a=10;
+//     console.log(a)
+// }
+// abc()
+// console.log(a)
+
+
+
+// let a=10
+// if(a==10){
+//     let b=20
+//     console.log(b)
+// }
+// console.log(b)
+
+
+// function abc(){
+//     const a =20;
+//     console.log(a)
+    
+// }
+// console.log(a)
+
+const a=10;
+if(a==10){
+    const b=20;
+    console.log(b)
+}
+ console.log(b)
