886f8197cb32aa70390027f04676cad731366989
palak-kaushal <kaushalpalak2005@gmail.com>
2025-06-27 13:43:00 +0530

    add files

---
+++ day4.js
@@ -0,0 +1,75 @@
+// a = 5 
+// b=4
+// console.log(a**b)
+
+//check the variable type 
+
+// console.log(5=="5")
+// console.log(5==="5")
+// console.log(5!="5")
+// console.log(5!=="5")
+// console.log(5>5)
+// console.log(5<5)
+// console.log(5>=5)
+// console.log(5<=5)
+
+//logical 
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || false)
+// console.log(!true)
+// console.log(!false)
+
+// const a= 10;
+// if(!!a){
+//    console.log("a is true") 
+// }else{
+//     console.log("a is true") 
+// }
+
+let button = document.getElementById("button")
+
+// button.onclick = function() {
+//     alert("using javascript property")
+// }
+
+// button.addEventListener("click", function(event) {
+//     console.log("javascript")
+//     console.log(event.target)
+//     console.log(event.type)
+//     console.log(event.key)
+//         // alert("javascript")
+// })
+
+// const input = document.getElementById("input")
+
+// input.addEventListener("keydown", function(event) {
+//     console.log(event.key)
+//     console.log(event.type)
+//     console.log(event.target)
+
+// })
+
+// button.addEventListener("click", pressbutton)
+// function pressbutton = function() {
+//     alert("javascript")
+// })
+
+
+// const cont = document.getElementById("container")
+
+// cont.addEventListener("mouseover", function() {
+//     cont.style.backgroundColor = "red"
+// })
+const btn =document.getElementById("btn")
+const ctn =document.getElementById("ctn")
+
+btn.addEventListener("click",function){
+    alert("btn is clicked")
+
+})
+ctn.addEventListener("click",function){
+    alert("ctn is clicked")
+})
+
+
