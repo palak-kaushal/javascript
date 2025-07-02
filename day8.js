7d5977845ef5d901c0acd23cdcf654de478929aa
palak-kaushal <kaushalpalak2005@gmail.com>
2025-07-02 14:39:01 +0530

    add file

---
+++ day8.js
@@ -0,0 +1,45 @@
+// let a =new Date()
+// console.log(a)
+
+// const b= new Date("2005","07",16)
+// console.log(b)
+
+// let a =new Date()
+
+// let b= a.getFullYear()
+// let c = a.getmonth()
+// let d= a.getdate()
+// let e= a.getDay()
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+
+// let counter =0;
+
+//  const id = setInterval(function(){
+//     counter=counter+1
+//     console.log(counter)
+// },50)
+
+// clearInterval(id)
+
+// setTimeout(function(){
+//     counter =counter*10
+//     console.log(counter)
+// },2000)
+
+// let a;
+// console.log(a)
+
+// let b= null;
+// console.log(b)
+
+let a;
+
+if (a===null){
+    console.log("a is value assign null")
+}else{
+    console.log("a is")
+}
