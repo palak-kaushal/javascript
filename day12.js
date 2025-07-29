41486e0cb3461dbe985124b65b3076d8836fb4db
palak-kaushal <kaushalpalak2005@gmail.com>
2025-07-29 14:17:14 +0530

    add files

---
+++ day12.js
@@ -0,0 +1,13 @@
+function outerFunction(){
+    let counter =0;
+    function innerFunction(){
+        counter++;
+        console.log(counter);
+    }
+    return innerFunction;
+    
+}
+const myClsure =outerFunction();
+myClsure();
+myClsure();
+myClsure();
